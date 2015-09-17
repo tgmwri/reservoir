363c85367e963c6551b0e7d9c4e202a7d6f5030a
Stanislav Horacek <stanislav.horacek@gmail.com>
2015-09-17 18:07:16 +0200

    add plots of probability fields

--- R/plots.R
+++ R/plots.R
@@ -14,6 +14,7 @@ prob_field <- function(reser, probs, yield, storage, throw_exceed) UseMethod("pr
 #' @return A \code{wateres_prob_field} object which is a list consisting of:
 #'   \item{quantiles}{data.table containing storage and yield values for months and given probabilities}
 #'   \item{probs}{given probability values in percent as characters}
+#' @seealso \code{\link{plot.wateres_prob_field}} for plotting the results
 #' @export
 #' @examples
 #' reser = data.frame(
@@ -40,7 +41,7 @@ prob_field.wateres <- function(reser, probs, yield, storage = attr(reser, "Vpot"
     quantiles = quantiles[, month := 1:12]
 
     for (var in c("storage", "yield")) {
-        tmp_pos = which(gsub(var, "", names(quantiles)) != names(quantiles))
+        tmp_pos = which(gsub(var, "", names(quantiles), fixed = TRUE) != names(quantiles))
         for (mon in 1:12)
             quantiles[month == mon, names(quantiles)[tmp_pos] := as.list(var_quant[[var]][[mon]])]
     }
@@ -48,4 +49,58 @@ prob_field.wateres <- function(reser, probs, yield, storage = attr(reser, "Vpot"
     resul = list(quantiles = quantiles, probs = prob_names)
     class(resul) = c("wateres_prob_field", class(resul))
     return(resul)
+}
+
+check_plot_pkgs <- function() {
+    for (pkg_name in c("ggplot2")) {
+        if (!requireNamespace(pkg_name, quietly = FALSE))
+            stop(paste0("To produce a plot, ", pkg_name, " package needs to be installed."))
+    }
+}
+
+#' Plot of probability field
+#'
+#' Plots monthly values of storage or yield stored in a given \code{wateres_prob_field} object, by using the \code{ggplot2} package.
+#'
+#' @param x A \code{wateres_prob_field} object.
+#' @param type Type of values to be plotted (\dQuote{storage} or \dQuote{yield}).
+#' @param filename A file name where the plot will be saved. If not specified, the plot will be printed to the current device.
+#' @param width Plot width in inches (or a unit specified by the \code{units} argument).
+#' @param height Plot height in inches (or a unit specified by the \code{units} argument).
+#' @param ... Further arguments passed to the \code{\link[ggplot2:ggsave]{ggsave}} function saving the plot to a file.
+#' @return A \code{ggplot} object.
+#' @export
+#' @examples
+#' reser = data.frame(
+#'     Q = c(0.078, 0.065, 0.168, 0.711, 0.154, 0.107, 0.068, 0.057, 0.07, 0.485, 0.252, 0.236,
+#'           0.498, 0.248, 0.547, 0.197, 0.283, 0.191, 0.104, 0.067, 0.046, 0.161, 0.16, 0.094),
+#'     DTM = seq(as.Date("2000-01-01"), by = "months", length.out = 24))
+#' reser = as.wateres(reser, Vpot = 14.4)
+#' prob_field = prob_field(reser, c(0.9, 0.95, 0.99), 0.14)
+#' plot(prob_field, "storage")
+plot.wateres_prob_field <- function(x, type = "storage", filename = NULL, width = 8, height = 6, ...) {
+    check_plot_pkgs()
+
+    types = c("storage", "yield")
+    units = c(storage = "mil. m\u00b3", yield = "m\u00b3.s\u207b\u00b9")
+    type = types[pmatch(type, types, 1)]
+    quant = copy(x$quantiles)
+    col_to_remove = names(quant) == gsub(type, "", names(quant), fixed = TRUE)
+    col_to_remove[1] = FALSE # month
+    quant = quant[, names(quant)[col_to_remove] := NULL]
+
+    mquant = reshape2::melt(quant, id = "month")
+    if (type == "storage")
+        mquant$value = mquant$value / 1e6
+
+    p = ggplot2::ggplot(mquant, ggplot2::aes(x = month, y = value, colour = variable)) + ggplot2::geom_line()
+    p = p + ggplot2::scale_x_discrete() + ggplot2::scale_y_continuous(paste0(type, " [", units[type], "]"))
+    p = p + ggplot2::scale_colour_discrete(name = "probability", labels = x$probs)
+    p = p + ggplot2::theme(legend.position = "bottom")
+
+    if (!is.null(filename))
+        ggplot2::ggsave(filename = filename, width = width, height = height, ...)
+    else
+        print(p)
+    return(p)
 }
---
+++ visual_test/prob_field.R
@@ -0,0 +1,13 @@
+vcontext("probability fields")
+
+library(ggplot2)
+
+riv = as.wateres("tests/testthat/rivendell.txt", 14.4)
+prob_field = prob_field(riv, c(0.9, 0.95, 0.99), 0.14)
+plot(prob_field, "storage")
+save_vtest("storage probability field")
+
+plot(prob_field, "yield")
+save_vtest("yield probability field")
+
+end_vcontext()
