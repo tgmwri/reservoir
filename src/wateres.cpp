5dfede392e613f65ab84b4b9235f3b83d291b5db
Stanislav Horacek <stanislav.horacek@gmail.com>
2015-10-19 17:37:17 +0200

    pass reservoir variables and attributes to C++ in data frame

--- src/wateres.cpp
+++ src/wateres.cpp
@@ -41,23 +41,30 @@ double convert_mm(double value, double area)
   return value / 1e3 * area;
 }
 
+//!variable names (inflow, evaporation, withdrawal, precipitation, yield)
+const string wateres::var_names[wateres::var_count] = {"Q", "E", "W", "P", "Y"};
+
 /**
  * - creates water reservoir from given vectors of variables and options
  */
 wateres::wateres(
-  vector<double> precipitation, vector<double> evaporation, vector<double> withdrawal, vector<double> yield,
-  vector<double> storage, vector<unsigned> days, DataFrame eas, bool throw_exceed, double volume, double area)
+  DataFrame reser, vector<double> storage, bool throw_exceed, double volume) : storage(storage),
+  throw_exceed(throw_exceed), volume(volume)
 {
-  this->precipitation = precipitation;
-  this->evaporation = evaporation;
-  this->withdrawal = withdrawal;
-  this->yield = yield;
-  this->storage = storage;
-  this->days = days;
-  this->eas = eas;
-  this->throw_exceed = throw_exceed;
-  this->volume = volume;
-  this->area = area;
+  unsigned row_count = reser.nrows();
+  vector<string> col_names = as<vector<string> >(reser.attr("names"));
+  var.resize(var_count);
+  for (unsigned v = 0; v < var_count; v++) {
+    if (find(col_names.begin(), col_names.end(), var_names[v]) != col_names.end()) {
+      var[v] = as<vector<double> >(reser[var_names[v]]);
+    }
+    else {
+      var[v].resize(row_count, 0);
+    }
+  }
+  this->days = as<vector<unsigned> >(reser[".days"]);
+  area = as<double>(reser.attr("area"));
+  eas = as<DataFrame>(reser.attr("eas"));
 }
 
 /**
@@ -79,22 +86,22 @@ double wateres::get_area(double storage_req)
       return (eas_area[as] - eas_area[as - 1]) / (eas_storage[as] - eas_storage[as - 1]) * (storage_req - eas_storage[as - 1]) + eas_area[as - 1];
     }
   }
+  return 0;
 }
 
 /**
  * - calculates reservoir water balance for a time step starting from given variable
  * - sequence evaporation -> yield -> withdrawal applied
- * @param variable variable to start with
  * @param ts time step to be calculated
- * @param var variable identification
+ * @param var_n identification of the variable to start with
  */
-void wateres::calc_balance_var(vector<double> &variable, unsigned ts, var_name var)
+void wateres::calc_balance_var(unsigned ts, var_name var_n)
 {
   int tmp_coeff = -1; //whether to add or subtract in the balance
-  switch (var) {
+  switch (var_n) {
     case PRECIPITATION:
       tmp_coeff = 1;
-      variable[ts] = convert_mm(variable[ts], area);
+      var[var_n][ts] = convert_mm(var[var_n][ts], area);
       break;
     case EVAPORATION:
       double tmp_area;
@@ -102,42 +109,42 @@ void wateres::calc_balance_var(vector<double> &variable, unsigned ts, var_name v
         tmp_area = area;
       else
         tmp_area = get_area(storage[ts]);
-      variable[ts] = convert_mm(variable[ts], tmp_area);
+      var[var_n][ts] = convert_mm(var[var_n][ts], tmp_area);
       break;
     default:
       break;
   }
-  storage[ts + 1] += variable[ts] * tmp_coeff;
+  storage[ts + 1] += var[var_n][ts] * tmp_coeff;
   if (storage[ts + 1] < 0) {
-    variable[ts] = variable[ts] + storage[ts + 1];
+    var[var_n][ts] = var[var_n][ts] + storage[ts + 1];
     storage[ts + 1] = 0;
-    switch (var) {
+    switch (var_n) {
       case EVAPORATION:
-        yield[ts] = 0;
-        withdrawal[ts] = 0;
+        var[YIELD][ts] = 0;
+        var[WITHDRAWAL][ts] = 0;
         break;
       case YIELD:
-        withdrawal[ts] = 0;
+        var[WITHDRAWAL][ts] = 0;
         break;
       default:
         break;
     }
   }
   else {
-    switch (var) {
+    switch (var_n) {
       case PRECIPITATION:
-        calc_balance_var(evaporation, ts, EVAPORATION);
+        calc_balance_var(ts, EVAPORATION);
         break;
       case EVAPORATION:
-        calc_balance_var(yield, ts, YIELD);
+        calc_balance_var(ts, YIELD);
         break;
       case YIELD:
-        calc_balance_var(withdrawal, ts, WITHDRAWAL);
+        calc_balance_var(ts, WITHDRAWAL);
         break;
       case WITHDRAWAL:
         if (storage[ts + 1] > volume) {
           if (!throw_exceed)
-            yield[ts] += storage[ts + 1] - volume;
+            var[YIELD][ts] += storage[ts + 1] - volume;
           storage[ts + 1] = volume;
         }
         break;
@@ -149,56 +156,48 @@ void wateres::calc_balance_var(vector<double> &variable, unsigned ts, var_name v
 
 /**
   * - calculates monthly time series of reservoir storage and yield
+  * @param Rreser reservoir object with time series of inflows (Q) in m3.s-1, precipitation (R) in mm, evaporation (E) in mm,
+    withdrawal in m3, number of days in months (.days) and with attributes (area - flooded by reservoir in m2, eas - elevation-area-storage
+    relationship (in m.a.s.l., m2 and m3)
   * @param Rinflow time series of inflows in m3.s-1
-  * @param Rdays number of days for months of time series
-  * @param Rprecipitation time series of precipitation in mm
-  * @param Revaporation time series of evaporation in mm
-  * @param Rwithdrawal time series of withdrawal in m3
   * @param Ryield_req required yield (reservoir outflow) in m3.s-1
   * @param Rvolume reservoir potential volume in m3
   * @param Rinitial_storage initial storage in the reservoir in m3
-  * @param Rarea area flooded by reservoir in m2
-  * @param Reas elevation-area-storage relationship (in m.a.s.l., m2 and m3)
   * @param Rthrow_exceed whether volume exceeding maximum storage will be thrown or added to yield
   * @return list consisting of storage (in m3), yield (m3.s-1), precipitation (m3), evaporation (m3) and withdrawal (m3)
   */
-RcppExport SEXP calc_storage(
-  SEXP Rinflow, SEXP Rdays, SEXP Rprecipitation, SEXP Revaporation, SEXP Rwithdrawal, SEXP Ryield_req, SEXP Rvolume, SEXP Rinitial_storage,
-  SEXP Rarea, SEXP Reas, SEXP Rthrow_exceed)
+RcppExport SEXP calc_storage(SEXP Rreser, SEXP Ryield_req, SEXP Rvolume, SEXP Rinitial_storage, SEXP Rthrow_exceed)
 {
-  vector<double> inflow = as<vector<double> >(Rinflow);
-  vector<unsigned> days = as<vector<unsigned> >(Rdays);
-  vector<double> precipitation = as<vector<double> >(Rprecipitation);
-  vector<double> evaporation = as<vector<double> >(Revaporation);
-  vector<double> withdrawal = as<vector<double> >(Rwithdrawal);
+  DataFrame reser = as<DataFrame>(Rreser);
   double yield_req = as<double>(Ryield_req);
-  DataFrame eas = as<DataFrame>(Reas);
   double volume = as<double>(Rvolume);
   double initial_storage = as<double>(Rinitial_storage);
-  double area = as<double>(Rarea);
   bool throw_exceed = as<bool>(Rthrow_exceed);
 
-  unsigned time_steps = inflow.size(), ts;
-  convert_m3(inflow, days, true);
-  vector<double> yield_req_vol(time_steps, yield_req);
-  convert_m3(yield_req_vol, days, true);
+  unsigned ts;
+  //reser.nrows() incorrect when subset of data.table used and its attributes are copied afterwards
+  //-> take length of arbitrary column which is not affected by the attributes
+  unsigned time_steps = as<NumericVector>(reser[".days"]).size();
 
-  vector<double> yield(time_steps, 0), storage(time_steps + 1, 0);
+  vector<double> storage(time_steps + 1, 0);
   storage[0] = initial_storage;
 
-  wateres reser(precipitation, evaporation, withdrawal, yield, storage, days, eas, throw_exceed, volume, area);
+  wateres reservoir(reser, storage, throw_exceed, volume);
+  vector<double> yield_req_vol(time_steps, yield_req);
+  convert_m3(yield_req_vol, reservoir.days, true);
+  convert_m3(reservoir.var[wateres::INFLOW], reservoir.days, true);
   for (ts = 0; ts < time_steps; ts++) {
-    reser.yield[ts] = yield_req_vol[ts];
-    reser.storage[ts + 1] = reser.storage[ts] + inflow[ts];
-    reser.calc_balance_var(reser.precipitation, ts, wateres::PRECIPITATION);
+    reservoir.var[wateres::YIELD][ts] = yield_req_vol[ts];
+    reservoir.storage[ts + 1] = reservoir.storage[ts] + reservoir.var[wateres::INFLOW][ts];
+    reservoir.calc_balance_var(ts, wateres::PRECIPITATION);
   }
   List resul;
-  resul["storage"] = reser.storage;
-  convert_m3(reser.yield, days, false);
-  resul["yield"] = reser.yield;
-  resul["precipitation"] = reser.precipitation;
-  resul["evaporation"] = reser.evaporation;
-  resul["withdrawal"] = reser.withdrawal;
+  resul["storage"] = reservoir.storage;
+  convert_m3(reservoir.var[wateres::YIELD], reservoir.days, false);
+  resul["yield"] = reservoir.var[wateres::YIELD];
+  resul["precipitation"] = reservoir.var[wateres::PRECIPITATION];
+  resul["evaporation"] = reservoir.var[wateres::EVAPORATION];
+  resul["withdrawal"] = reservoir.var[wateres::WITHDRAWAL];
 
   return resul;
 }
--- src/wateres.h
+++ src/wateres.h
@@ -7,18 +7,18 @@
 class wateres
 {
   public:
-    wateres(
-      std::vector<double> precipitation, std::vector<double> evaporation, std::vector<double> withdrawal, std::vector<double> yield,
-      std::vector<double> storage, std::vector<unsigned> days, Rcpp::DataFrame eas, bool throw_exceed, double volume, double area); //!< reservoir from given values
+    wateres(Rcpp::DataFrame reser, std::vector<double> storage, bool throw_exceed, double volume); //!< reservoir from given values
 
     //! water balance variables
-    enum var_name {PRECIPITATION, EVAPORATION, YIELD, WITHDRAWAL};
-    //! values of water balance variables
-    std::vector<double> precipitation, evaporation, withdrawal, yield, storage;
+    enum var_name {INFLOW, EVAPORATION, WITHDRAWAL, PRECIPITATION, YIELD};
+    static const unsigned var_count = 5; //!< number of time series of variables
+    static const std::string var_names[]; //!< names of variables
+    std::vector<std::vector<double> > var; //!< values of water balance variables
+    std::vector<double> storage;
     std::vector<unsigned> days; //!< number of days for time steps
     Rcpp::DataFrame eas; //!< elevation-area-storage relationship (m, m2 and m3)
 
-    void calc_balance_var(std::vector<double> &variable, unsigned ts, var_name var); //!< calculates water balance for given variable and time step
+    void calc_balance_var(unsigned ts, var_name var); //!< calculates water balance for given variable and time step
 
   private:
     bool throw_exceed; //!< whether volume exceeding maximum storage will be thrown
