599dcdb72df28e57ed2bb5e27cba013c8d66c6d6
Stanislav Horacek <stanislav.horacek@gmail.com>
2016-04-28 12:36:18 +0200

    add test for system of reservoir

--- tests/testthat/test_system.R
+++ tests/testthat/test_system.R
@@ -51,4 +51,43 @@ test_that("system is adjusted after check", {
     expect_equivalent(check(sys), readRDS("system_check.rds"))
 })
 
+riv_wateruse = 4e5
+riv_data = read.table("rivendell.txt", colClasses = c("Date", "numeric"), header = TRUE)
+riv_data = riv_data[riv_data$DTM > as.Date("1981-01-01"), ]
+riv = as.wateres(riv_data, 14.4e6, 754e3, id = "A1", down_id = "B")
+riv = set_withdrawal(riv, rep(riv_wateruse, nrow(riv)))
+riv_mrf = 0.033
 
+riv_paralel = as.wateres(riv_data, 14.4e6, 754e3, id = "A2", down_id = "B")
+riv_paralel = set_withdrawal(riv_paralel, rep(riv_wateruse, nrow(riv_paralel)))
+
+riv2_wateruse = riv_wateruse * 1.12
+riv2_data = riv_data
+riv2_data$Q = riv2_data$Q * 1.12
+riv2 = as.wateres(riv2_data, 14.4e6, 754e3, id = "B", down_id = "C")
+riv2 = set_withdrawal(riv2, rep(riv2_wateruse, nrow(riv2)))
+riv2_mrf = 0.033
+
+thar_wateruse = 1e7
+thar_data = read.table("tharbad.txt", colClasses = c("Date", "numeric"), header = TRUE)
+thar_data = thar_data[thar_data$DTM > as.Date("1981-01-01"), ]
+thar = as.wateres(thar_data, 41.3e6, 2672e3, id = "C")
+thar = set_withdrawal(thar, rep(thar_wateruse, nrow(thar)))
+thar_mrf = 2.718
+
+test_that("system is not calculated without yields", {
+    system = as.system(riv, riv_paralel, riv2, thar)
+    yields = c(A1 = riv_mrf, A2 = riv_mrf, B3 = riv2_mrf, C = thar_mrf)
+    expect_error(calc_deficits(system, yields), "Yields are not provided")
+})
+
+test_that("deficits for system of four reservoirs are calculated", {
+    system = as.system(riv, riv_paralel, riv2, thar)
+    yields = c(A1 = riv_mrf, A2 = riv_mrf, B = riv2_mrf, C = thar_mrf)
+    expect_equivalent(calc_deficits(system, yields), readRDS("system4_deficits.rds"))
+
+    riv2$W[108] = riv2$W[108] + 5845586.9 # increase water use to get deficit 9e5 in reservoir B
+    system = as.system(riv, riv_paralel, riv2, thar)
+    yields = c(A1 = riv_mrf, A2 = riv_mrf, B = riv2_mrf, C = thar_mrf)
+    expect_equivalent(calc_deficits(system, yields), readRDS("system4_deficits_B.rds"))
+})
