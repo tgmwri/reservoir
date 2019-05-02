79a8d727785921ab99e280990c4389f9a90127d4
Stanislav Horacek <stanislav.horacek@gmail.com>
2019-05-02 11:33:32 +0200

    add initial calculation of system defined by catchments

---
+++ R/catchment.R
@@ -0,0 +1,97 @@
+# data - DTM, R, PET
+# area - in km2
+as.catchment <- function(id, down_id, data, area, res_data) {
+    # TDD check data, res_data
+    data$Q = data$R * 1e3 * area / (24 * 3600) # TDD general time step, dtto as.wateres
+
+    res_branches = list(main = res_data[res_data$is_main,], other = res_data[!res_data$is_main,])
+    res_branches = lapply(res_branches, function(res_dframe) {
+        res_dframe = res_dframe[order(res_dframe$part),]
+        res_dframe$down_id = NA
+        for (res in 1:nrow(res_dframe)) {
+            if (res + 1 <= nrow(res_dframe)) {
+                res_dframe[res, "down_id"] = paste0(id, "_", res_dframe[res + 1, "id"])
+            }
+            else {
+                res_dframe[res, "down_id"] = paste0(id, "_outlet") # TDD check reserved keyword
+            }
+        }
+        return(res_dframe)
+    })
+    
+    reservoirs = list()
+    for (branch in c("main", "other")) {
+        res_branch = res_branches[[branch]]
+        for (res in 1:nrow(res_branch)) {
+            curr_res = res_branch[res,]
+            curr_res$id = paste0(id, "_", curr_res$id)
+            curr_res_ts = data.frame(DTM = data$DTM, Q = data$Q * curr_res$part)
+            reservoirs[[curr_res$id]] = as.wateres(curr_res_ts, curr_res$storage, curr_res$area, time_step = "day", id = curr_res$id, down_id = curr_res$down_id)
+            reservoirs[[curr_res$id]] = set_evaporation(reservoirs[[curr_res$id]], data$PET)
+            attr(reservoirs[[curr_res$id]], "is_main") = branch == "main"
+            # TDD set wateruse etc.
+        }
+    }
+    reservoirs[[paste0(id, "_outlet")]] = as.wateres(data.frame(DTM = data$DTM, Q = data$Q), 0, 0, time_step = "day")
+    attr(reservoirs[[paste0(id, "_outlet")]], "is_main") = TRUE
+    attr(reservoirs[[paste0(id, "_outlet")]], "id") = paste0(id, "_outlet")
+    attr(reservoirs[[paste0(id, "_outlet")]], "down_id") = NA
+    attr(reservoirs, "id") = id
+    attr(reservoirs, "down_id") = down_id
+    if (length(res_branches$main) > 0) {
+        first_main_res = res_branches$main$id[1]
+    }
+    else {
+        first_main_res = "outlet"
+    }
+    attr(reservoirs, "first_main_res") = paste0(id, "_", first_main_res)
+    class(reservoirs) = c("catchment", "list")
+    return(reservoirs)
+}
+
+as.catchment_system <- function(...) {
+    catchments = list(...) # TDD check unique catchment ID, reservoir names
+    down_ids_to_process = c()
+    for (catch in 1:length(catchments)) {    
+        names(catchments)[catch] = attr(catchments[[catch]], "id")
+        if (!is.na(attr(catchments[[catch]], "down_id"))) {
+            down_ids_to_process = c(down_ids_to_process, attr(catchments[[catch]], "down_id"))
+        }
+    }
+    
+    processed_catchs = c()
+    while (length(processed_catchs) < length(catchments)) {
+        for (catch in 1:length(catchments)) {
+            curr_catch_id = attr(catchments[[catch]], "id")
+            curr_catch_down_id = attr(catchments[[catch]], "down_id")
+            if (!(curr_catch_id %in% down_ids_to_process)) {
+                curr_Q_outlet = catchments[[curr_catch_id]][[paste0(curr_catch_id, "_outlet")]]$Q
+                for (res_down in names(catchments[[curr_catch_down_id]])) {
+                    if (attr(catchments[[curr_catch_down_id]][[res_down]], "is_main")) {
+                        catchments[[curr_catch_down_id]][[res_down]]$Q = catchments[[curr_catch_down_id]][[res_down]]$Q + curr_Q_outlet
+                    }
+                }
+                if (!is.na(attr(catchments[[catch]], "down_id"))) {
+                    attr(catchments[[curr_catch_id]][[paste0(curr_catch_id, "_outlet")]], "down_id") = attr(catchments[[curr_catch_down_id]], "first_main_res")                
+                }
+                
+                down_ids_to_process = down_ids_to_process[-which(down_ids_to_process == curr_catch_down_id)[1]]
+                processed_catchs = c(processed_catchs, curr_catch_id)
+            }
+        }
+    }
+    all_reservoirs = list()
+    for (catch in 1:length(catchments)) {    
+        for (res in 1:length(catchments[[catch]])) {
+            all_reservoirs[[names(catchments[[catch]])[res]]] = catchments[[catch]][[res]]
+        }
+    }  
+    names(all_reservoirs) = NULL
+    result = do.call(as.system, all_reservoirs)
+    class(result) = c("catchment_system", "wateres_system", "list")
+    return(result)
+}
+
+calc_catchment_system <- function(system, yields) {
+    return(calc_system(system, yields, types = "system_plain"))
+}
---
+++ tests/testthat/test_catchment.R
@@ -0,0 +1,27 @@
+context("calculation of system of reservoirs defined by catchments")
+
+test_that("simple system of catchment reservoirs is calculated", {
+    data_catch = data.frame(DTM = seq(as.Date("1982-11-01"), length.out = 7, by = "day"), PET = rep(0.5, 7), R = rep(24 * 3.6, 7))
+    res_data_c1 = data.frame(
+        storage = c(1e7, 1e7, 1e7), area = c(1e2, 1e2, 1e2), part = c(0.25, 0.25, 0.5), is_main = c(TRUE, FALSE, FALSE), id = c("M1", "L1", "L2"),
+        stringsAsFactors = FALSE)
+    res_data_c2 = res_data_c1
+    res_data_c2$storage = res_data_c2$storage * 2
+
+    catch1 = as.catchment(id = "C1", down_id = "C2", data = data_catch, area = 100, res_data = res_data_c1)
+    catch2 = as.catchment(id = "C2", down_id = NA, data = data_catch, area = 200, res_data = res_data_c2)
+    catch_system = as.catchment_system(catch1, catch2)
+
+    yields = c(C1_M1 = 25, C1_L1 = 25, C1_L2 = 25, C1_outlet = 25, C2_M1 = 25, C2_L1 = 25, C2_L2 = 200, C2_outlet = 25)
+    resul = calc_catchment_system(catch_system, yields)
+    resul = resul$system_plain
+
+    expect_equal(as.data.frame(resul$C1_M1), data.frame(inflow = rep(25, 7), storage = rep(1e7, 7), yield = rep(25, 7), precipitation = rep(0, 7), evaporation = rep(0.05, 7), wateruse = rep(0, 7), deficit = rep(0, 7)), tolerance = 1e-5)
+    expect_equal(as.data.frame(resul$C1_L1), as.data.frame(resul$C1_M1))
+    expect_equal(as.data.frame(resul$C1_L2), data.frame(inflow = rep(50, 7), storage = rep(1e7, 7), yield = rep(50, 7), precipitation = rep(0, 7), evaporation = rep(0.05, 7), wateruse = rep(0, 7), deficit = rep(0, 7)), tolerance = 1e-5)
+    expect_equal(as.data.frame(resul$C1_outlet), data.frame(inflow = rep(100, 7), storage = rep(0, 7), yield = rep(100, 7), precipitation = rep(0, 7), evaporation = rep(0, 7), wateruse = rep(0, 7), deficit = rep(0, 7)), tolerance = 1e-5)
+    expect_equal(as.data.frame(resul$C2_M1), data.frame(inflow = rep(150, 7), storage = rep(2e7, 7), yield = rep(150, 7), precipitation = rep(0, 7), evaporation = rep(0.05, 7), wateruse = rep(0, 7), deficit = rep(0, 7)), tolerance = 1e-5)
+    expect_equal(as.data.frame(resul$C2_L1), data.frame(inflow = rep(50, 7), storage = rep(2e7, 7), yield = rep(50, 7), precipitation = rep(0, 7), evaporation = rep(0.05, 7), wateruse = rep(0, 7), deficit = rep(0, 7)), tolerance = 1e-5)
+    expect_equal(as.data.frame(resul$C2_L2), data.frame(inflow = rep(100, 7), storage = c(11.36e6, 2.72e6, rep(0, 5)), yield = c(rep(200, 2), 131.4815, rep(100, 4)), precipitation = rep(0, 7), evaporation = rep(0.05, 7), wateruse = rep(0, 7), deficit = c(rep(0, 2), 5.92e6, rep(8.64e6, 4))), tolerance = 1e-5)
+    expect_equal(as.data.frame(resul$C2_outlet), data.frame(inflow = c(rep(400, 2), 331.4815, rep(300, 4)), storage = rep(0, 7), yield = c(rep(400, 2), 331.4815, rep(300, 4)), precipitation = rep(0, 7), evaporation = rep(0, 7), wateruse = rep(0, 7), deficit = rep(0, 7)), tolerance = 1e-5)
+})
